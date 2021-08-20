78fc1f9f6ad4c60a2b092ae218132137d68f75ec
Awkeh <nicolas.mg.uy@gmail.com>
2021-08-19 22:15:06 -0300

    Added function to exchange temporal auth token for an access token to notion user's databases and pages. Code clean up

--- bg-worker.js
+++ bg-worker.js
@@ -1,22 +1,33 @@
-const OAUTH_URL = "https://api.notion.com/v1/oauth/authorize";
+const AUTHORIZE_URL = "https://api.notion.com/v1/oauth/authorize";
+const ACCESS_TOKEN_URL = "https://api.notion.com/v1/oauth/token";
 const CLIENT_ID = "";
 const REDIRECT_URL = chrome.identity.getRedirectURL();
 const DEBUG_MODE = true;
 
+function log(str) {
+	if (DEBUG_MODE) console.log(str);
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-	if (DEBUG_MODE) console.log("Redirect url:" + REDIRECT_URL);
+	log("Redirect url:" + REDIRECT_URL);
 });
 
 function authorize(sendResponse) {
 	chrome.identity
 		.launchWebAuthFlow(
 			{
-				url: `${OAUTH_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`,
+				url: `${AUTHORIZE_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`,
 				interactive: true,
 			},
 			(redirect_url) => {
-				sendResponse(redirect_url);
-				if (DEBUG_MODE) console.log("Response sent");
+				let urlObj = new URL(redirect_url);
+				let response = {
+					code: urlObj.searchParams.get("code"),
+					error: urlObj.searchParams.get("error")
+				};
+
+				sendResponse(response);
+				log("Response sent");
 			}
 		);
 
@@ -25,15 +36,38 @@ function authorize(sendResponse) {
 	}
 }
 
+async function requestAccess(authData, sendResponse) {
+	let cred = /* {client_id}:{client_secret} encoded in base64*/ 0;
+	await fetch(ACCESS_TOKEN_URL, {
+		mode: 'no-cors',
+		method: "POST",
+		headers: {
+			"Authorization": `Basic ${cred}`,
+			"Content-Type": "application/json"
+		},
+		body: {
+			grant_type: "authorization_code",
+			code: authData.code,
+			redirect_uri: REDIRECT_URL
+		}
+	})
+		.then((response) => response.json())
+		.then((data) => log);
+}
+
 function messageListener(request, sender, sendResponse) {
-	if (request.message === "login") {
-		authorize(sendResponse);
-		if (DEBUG_MODE) console.log("Auth executed");
-		return true;
-	}
-	else {
-		console.log(request.message);
-		return true;
+	switch (request.message) {
+		case "auth":
+			authorize(sendResponse);
+			log("Auth executed");
+			return true;
+		case "request-access":
+			requestAccess(request.authData, sendResponse);
+			log("Access request executed")
+			return true;
+		default:
+			log(request.message);
+			return true;
 	}
 }
 
--- popup-sign-in.js
+++ popup-sign-in.js
@@ -1,9 +1,17 @@
-function doStuffWithToken(token) {
-	chrome.runtime.sendMessage({ message: "Response recieved: " + token }, console.log);
+function handleAuthResponse(response) {
+	if(response.code == null) return;
+	//if(response.code != null) {} // if permission was denied
+
+	// TODO: delete
+	chrome.runtime.sendMessage({ message: "Response recieved: " + response.code }, console.log);
+
+	chrome.runtime.sendMessage({ message: "request-access", authData: response}, (res) => {
+		chrome.runtime.sendMessage({ message: "access requester: response recieved: " + JSON.stringify(res) }, console.log);
+	});
 }
 
 function clickHandler(evt) {
-	chrome.runtime.sendMessage({ message: "login" }, doStuffWithToken);
+	chrome.runtime.sendMessage({ message: "auth" }, handleAuthResponse);
 }
 
 btnLogin = document.getElementById("btnLogin");
